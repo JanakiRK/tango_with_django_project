928f2e1834de177c13d6d41e42580edb6b8b4674
Janaki RK <janaki26794@gmail.com>
2019-02-09 13:14:12 +0000

    eighth commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -87,6 +87,11 @@ DATABASES = {
 }
 
 
+#Password Hashing functions
+PASSWORD_HASHERS = (
+'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -96,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -125,6 +131,8 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/rango/login/'
 
 MEDIA_ROOT = MEDIA_DIR
 
